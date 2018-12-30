bd62d5b3ebc95c21420261f34a8d92ce18172f68
minhnhatvdl <minhnhatvdl@gmail.com>
2018-12-30 12:19:39 +0100

    set all components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+
+import Modal from './Components/Modal';
+import Control from './Components/Control';
+import Task from './Components/Task';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <h1 className="text-center my-2">Workflow Management</h1>
+        <div className="container-fluid">
+          <div className="row">
+            {/* PANEL */}
+            <Control />
+            {/* DISPLAY */}
+            <Task />
+          </div>
+        </div>
+        {/* The Modal */}
+        <Modal />
       </div>
     );
   }
